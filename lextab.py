605bb4fe54a4078a370e70e46ced88ec6633cdbe
karimalaa22 <karimalaa200@gmail.com>
2019-06-22 03:44:39 +0200

    added my files

---
+++ lextab.py
@@ -0,0 +1,10 @@
+# lextab.py. This file automatically created by PLY (version 3.9). Don't edit!
+_tabversion   = '3.8'
+_lextokens    = set(('FALSE', 'VARIABLE', 'CLOSE_CURLY_BRACE', 'CLASS', 'PRINT', 'NEW', 'SEMICOLON', 'START_CODE', 'DIVIDE', 'EQUALS', 'PLUS', 'DOUBLE_QUOTE', 'ARROW', 'RETURN', 'OPEN_CURLY_BRACE', 'COMMA', 'DOT', 'MINUS', 'OPEN_BRACE', 'TRUE', 'IDENTIFIER', 'SINGLE_QUOTE', 'TIMES', 'END_CODE', 'CLOSE_BRACE', 'FUNCTION', 'CONST'))
+_lexreflags   = 0
+_lexliterals  = ''
+_lexstateinfo = {'INITIAL': 'inclusive'}
+_lexstatere   = {'INITIAL': [('(?P<t_CLASS>\\bclass\\b)|(?P<t_FUNCTION>\\bfunction\\b)|(?P<t_PRINT>\\bprint\\b)|(?P<t_RETURN>\\breturn\\b)|(?P<t_NEW>\\bnew\\b)|(?P<t_TRUE>\\btrue\\b)|(?P<t_FALSE>\\bfalse\\b)|(?P<t_ARROW>\\->\\b)|(?P<t_newline>\\n+)|(?P<t_VARIABLE>[$][a-zA-Z_][a-zA-Z_0-9]*)|(?P<t_IDENTIFIER>[a-zA-Z_][a-zA-Z_0-9]*)|(?P<t_START_CODE>[<][?]php)|(?P<t_CONST>[0-9]+)|(?P<t_END_CODE>\\?>)|(?P<t_EQUALS>\\=)|(?P<t_CLOSE_CURLY_BRACE>\\})|(?P<t_TIMES>\\*)|(?P<t_DOT>\\.)|(?P<t_SINGLE_QUOTE>\\\')|(?P<t_PLUS>\\+)|(?P<t_CLOSE_BRACE>\\))|(?P<t_OPEN_BRACE>\\()|(?P<t_OPEN_CURLY_BRACE>\\{)|(?P<t_DIVIDE>/)|(?P<t_SEMICOLON>;)|(?P<t_DOUBLE_QUOTE>")|(?P<t_COMMA>,)|(?P<t_MINUS>-)', [None, ('t_CLASS', 'CLASS'), ('t_FUNCTION', 'FUNCTION'), ('t_PRINT', 'PRINT'), ('t_RETURN', 'RETURN'), ('t_NEW', 'NEW'), ('t_TRUE', 'TRUE'), ('t_FALSE', 'FALSE'), ('t_ARROW', 'ARROW'), ('t_newline', 'newline'), (None, 'VARIABLE'), (None, 'IDENTIFIER'), (None, 'START_CODE'), (None, 'CONST'), (None, 'END_CODE'), (None, 'EQUALS'), (None, 'CLOSE_CURLY_BRACE'), (None, 'TIMES'), (None, 'DOT'), (None, 'SINGLE_QUOTE'), (None, 'PLUS'), (None, 'CLOSE_BRACE'), (None, 'OPEN_BRACE'), (None, 'OPEN_CURLY_BRACE'), (None, 'DIVIDE'), (None, 'SEMICOLON'), (None, 'DOUBLE_QUOTE'), (None, 'COMMA'), (None, 'MINUS')])]}
+_lexstateignore = {'INITIAL': ' \t'}
+_lexstateerrorf = {'INITIAL': 't_error'}
+_lexstateeoff = {}
